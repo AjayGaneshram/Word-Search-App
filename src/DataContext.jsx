f0fe3daa05fb52342d21cb980bb882bcc7f61129
AjayGaneshram <95750604+AjayGaneshram@users.noreply.github.com>
2025-02-02 18:35:02 +0530

    Update DataContext.jsx

--- src/DataContext.jsx
+++ src/DataContext.jsx
@@ -1,37 +1,27 @@
 import React, { createContext, useState, useEffect } from "react";
-import jsonData from '../public/Output'
 export const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
   const [outputJson, setOutputJson] = useState(null);
-  const [searchResults, setSearchResults] = useState(null); 
+  const [searchResults, setSearchResults] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
-      // fetch("./output.json")
-      //   .then((response) => {
-      //     return response.json();
-      //   })
-      //   .then((data) => {
-			// console.log(data)
-          setOutputJson(jsonData);
-        // })
-        // .catch((error) => console.error("Error fetching JSON:", error));
-
-
-        import('./output.js')
-            .then((module) => {
-              setOutputJson(module.default);
-            })
-            .catch((error) => {
-                console.error('Error loading data:', error);
-            });
+      import("./output.js")
+        .then((module) => {
+          setOutputJson(module.default);
+        })
+        .catch((error) => {
+          console.error("Error loading data:", error);
+        });
     };
 
     fetchData();
   }, []); // empty dependency array to run the fetch only once on mount
 
   return (
-    <DataContext.Provider value={{ outputJson ,searchResults, setSearchResults}}>
+    <DataContext.Provider
+      value={{ outputJson, searchResults, setSearchResults }}
+    >
       {children}
     </DataContext.Provider>
   );
